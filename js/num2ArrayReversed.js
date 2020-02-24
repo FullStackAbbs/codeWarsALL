e26b5cf5506e8aca236c6b296672c2b7e9eb20fb
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-02-24 16:34:22 -0500

    yes

---
+++ js/num2ArrayReversed.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051/javascript
+//
+// Convert number to reversed array of digits
+// Given a random number:
+
+//
+// You have to return the digits of this number within an array in reverse order.
+//
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// PREP
+// Parameters user input to be reversed
+// Return an array reversed in order
+// Example 348597 => [7,9,5,8,4,3]
+// Psuedo Code (x) take in the number into an array (x) input number into an array (x)spereate the elements of the array
+//  (x)  reverse the order of the array elements (x) rejoin the elements  (x) have the elements printed
+var inputNum=23524;
+var num=['${inputNum}']
+// (x) user input string needs to be attained
+function flippedString(num){
+  var numberSet=str.spilt('');
+  newNumbetSet= numberSet.reverse();
+  newNumberrSet=numberSet.join('');
+  return newNumbetSet;
+}
