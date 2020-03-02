80c7286b64944cd07f14044433664fd71a148852
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-02 10:49:36 -0500

    yes

---
+++ js/negative.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+
+// PREP
+// Parameters a number
+// Return -N
+// Example makeNegative(1); // return -1
+// Psuedo Code (x) take in a number (x) functions
+
+var n =4;
+
+function makeNegative(n){
+  return -${n}
+}
