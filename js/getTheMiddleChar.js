95c3368284b63b55fc232a1ce8e0ca7d0de34cc6
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-11 22:50:43 -0400

    answer

--- js/getTheMiddleChar.js
+++ js/getTheMiddleChar.js
@@ -1,2 +1,12 @@
 // Get The Middle Character
-// (x) if length is 
+// (x) if length is
+
+function sqauredNumber(num){
+  let strNum = string(num);
+  let squaredNumber="";
+  strNum.forEach(el => {
+    let sqEl = Math.pow(parseInt(el),2)
+  })
+  result = result + string(sqauredNumber)
+  reutrn parseInt(result)
+}
