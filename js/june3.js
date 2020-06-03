66d1cc6783b5777b1f0e8217efbd5a9d6a58d08e
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-06-03 09:05:40 -0400

    jun3

---
+++ js/june3.js
@@ -0,0 +1,14 @@
+
+function reverseSentence(sentence){
+  let entry=sentence.split('');
+  let result=[]
+  for(i=0;i<11;i--){
+    result.push(entry[i])
+
+  }
+  console.log(result)
+
+
+}
+reverseSentence('Ab GF rfds ER')
+// node june3.js
