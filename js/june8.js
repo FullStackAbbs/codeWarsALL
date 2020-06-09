3cc80dc1ad736f02559b23e9b0655bbc1672ce19
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-06-09 09:35:35 -0400

    jun9

--- js/june8.js
+++ js/june8.js
@@ -6,17 +6,22 @@
 // Psuedocode
 // create a new string based on the fact whether the character is already in the output string
 function removeDuplicate(input){
-  let output="aaba"
-  for (let i=0;i < input.length; ){
-    if ( output.includes('a' ){
-        i++
-    }else{
-      output =+ input[i]
-
-    }
-    // console.log(output.includes(input[i]), [i])
+  //attain the input string
+  // iterate thru each character of the string
+  // check if the character on the pointer is in the output value
+  // if false: it should add the character to the output variable
+  // if true: go the next iterated index
+  // once the loop is completed return the output value
+let output = '';
+for (i=0;i < input.length; ){
+  if(output.includes(input[i])){
+    i++
+  }else{
+    output += input[i]
   }
-  return output
+  // console.log(input[i])
+}
+return output
 
 }
 console.log(removeDuplicate('abigail'))
