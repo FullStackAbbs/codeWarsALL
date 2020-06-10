8d32fd6e212f0dfe76d26d169b6af8e356fe1413
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-06-10 09:07:02 -0400

    june9

---
+++ js/june9.js
@@ -0,0 +1,23 @@
+// Question: How would you remove duplicate members from an array?
+// Parameters an array
+// Return no duplicates
+// PsuedoCode
+// iterate through the elements (// seperate the elements)
+// create an output arr for the no duplicates
+// based on the T/F result of a comparison between the output arr and currently input array element
+// if the indexOf the element is -1 then add it to the output array
+// if it indexOf the element is greater than -1 = 0,1,2... then go the next element
+
+
+function removeDupArr(arr){
+let output=[];
+arr.forEach((el) =>{
+  if(output.indexOf(el) === -1){
+    output.push(el)
+  }
+})
+return output
+
+}
+console.log(removeDupArr([2,3,2,3,4,5,1,4,78,43,23,23]))
+// node june9.js
