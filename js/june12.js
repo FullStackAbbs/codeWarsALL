0714b98d9a589f2d8bc5b60069d8f296a6e1f6b8
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-06-13 15:33:28 -0400

    Create june12.js

---
+++ js/june12.js
@@ -0,0 +1,6 @@
+// Count the number of zeros from 1 to n
+// Parameter the number (n)
+// Return count of zeros from 1 to n
+// Example
+// n=10 , 10, count:1  n/10 = Count
+// n=123, 10,20,30,40,50,60,70,80,90,100,110,120, count: 13 n/10= count+1 (discard the decimal)
