47dab0d31b591c7c0b36ec8df6f9e50799d70cd6
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-02 22:27:28 -0400

    jul2

---
+++ js/jul2.js
@@ -0,0 +1,13 @@
+// Life Path Calc for M.R.
+
+let negCount=0;
+function countNegatives(grid){
+  grid.forEach((row) =>
+
+  row.forEach((el)=> el < 0 ? negCount++ : null
+
+   ))
+   return console.log(negCount)
+}
+
+countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]])
