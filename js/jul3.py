946412f5f75d9538c36b27f7ec3f657e7ca489b5
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-03 22:11:54 -0400

    jul3

---
+++ js/jul3.py
@@ -0,0 +1,36 @@
+count = 3
+name = 'abbey'
+text = "abbey, my favoritue programmer"
+
+
+
+def areaOfCircle(r): #def is the keyword to say it's a function
+    pi = 3.1453
+    area = pi * (r ** 2)
+    return area
+
+# a = int(input())
+# b = int(input())
+res = areaOfCircle(5) #calling a function, taking in two input
+print(res)
+
+
+
+# python jul3.py
+##########  NOTES ##########
+
+# print(type(text)) //str,int float
+
+# str() changes data to a string.
+# int() changes data to an integer.
+# float() changes data to a float
+
+# print('Division (float) : ', a / b)
+# print('Division (floor) : ', a // b)
+# print('Modulus : ', a % b)
+# print('Exponent : ', a ** b)
+
+# upper() changes the string to upper case.
+# lower() changes the string to lower case.
+# capitalize() capitalizes a string's first word.
+# title() capitalizes every word in a string.
