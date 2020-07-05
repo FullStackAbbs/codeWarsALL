dee02fa1b434479b559d3fc0798b81d674ef73cf
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-04 21:34:26 -0400

    JUL4

---
+++ js/PythonNote.py
@@ -0,0 +1,42 @@
+##########  NOTES ##########
+
+# print(type(text)) //str,int float
+
+# str() changes data to a string.
+# int() changes data to an integer.
+# float() changes data to a float
+
+# print('Division (float) : ', a / b)
+# print('Division (floor) : ', a // b)
+# print('Modulus : ', a % b)
+# print('Exponent : ', a ** b)
+
+# upper() changes the string to upper case.
+# lower() changes the string to lower case.
+# capitalize() capitalizes a string's first word.
+# title() capitalizes every word in a string.
+
+
+#Slicing:
+# print(msg[a:]) returns everything after the character in the position specified.
+# print(msg[a:b]) returns everything between the positions specified, not including the end position.
+# print(msg[:b]) returns everything until the position specified, again, not including the end position.
+
+#Inputs
+# name= input('What is your name?: ')
+# print(name)
+
+#List:
+# print(friends[2]) regular indexing smimilar to js arrays
+# len( *what you checking the length of*) how many characters the string
+# print(msg.count('Python')) counts the number of instances of a word or letter - note that this is case sensitive.
+
+# def areaOfCircle(r): #def is the keyword to say it's a function
+#     pi = 3.1453
+#     area = pi * (r ** 2)
+#     return area
+# res = areaOfCircle(5) #calling a function, taking in two input
+# print(res)
+
+for number in range(2,8):
+    print(number)
---
+++ js/jul4.py
@@ -0,0 +1,27 @@
+# Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+#
+# Note: numbers and their corresponding string representations should not be treated as duplicates (i.e., "1" != 1).
+#
+# Examples
+# [1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+# [0, 1, 2, 3, 4, 5]
+
+# Input: an array that may have numbers and strings
+# Output: an array of duplicates
+# Example: [1, 2, 4, 4, 3, 3, 1, 5, 3, '5'] should give us 4,3,1 bc the count and times it appears greater than one
+# Psuedocode:
+# iterate through every element
+# check how many time the element appears
+# if the element appears more the once:
+## add the element to the output list
+output=[]
+def duplicates(array):
+    for element in array:
+        if array.count(element) > 1 and element not in output:
+            output.append(element)
+    return output
+res = duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5'])
+print(res)
+# python jul4.py
+# line 19 O(n) iteration
+# ## line 21 O(1)
