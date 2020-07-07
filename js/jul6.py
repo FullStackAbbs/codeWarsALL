b11334a2193c41d7f53d7921ead1f7e70b7a59be
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-07-07 00:51:48 -0400

    Update jul6.py

--- js/jul6.py
+++ js/jul6.py
@@ -6,3 +6,27 @@
  # 60 <= score < 70	'D'
  # 0 <= score < 60	'F'
  # Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100
+ # Input 3 number array
+ # Output a string A to F
+ # Example
+ # [90,100,80]
+ # sum these numbers and divide by the number of elements
+ # check the range of the avg number and return the corresponding letter
+
+
+def get_grade(s1,s2,s3):
+    list=[s1,s2,s3]
+    avgGrade =sum(list)/len(list)
+    if 90 <=avgGrade<= 100:
+        return 'A'
+    if 80 <=avgGrade<= 90:
+        return 'B'
+    if 70 <=avgGrade<= 80:
+        return 'C'
+    if 60 <=avgGrade<= 70:
+        return 'D'
+    if 0 <=avgGrade<= 60:
+        return 'F'
+res = get_grade(95,0,93) #calling a function, taking in two input
+print(res)
+# python jul6.py
