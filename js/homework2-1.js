96837652d6d82aed84ba7a3f649b988437cd70fb
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-10-06 23:30:22 -0400

    update

---
+++ js/homework2-1.js
@@ -0,0 +1,14 @@
+// build a function that takes in an array of strngs and returns an array for each array item
+//it returns one array that has two items
+//one is a boolean - are there anagrams in this array
+//and the other is how many
+​
+//['raey', 'year'] -> [true, 1] // cause there are only one pair
+//return new array with 1st value being true and 2nd calue being 2/however many anagrams
+​
+// Input (An array)
+// Output (an array with two values: 1)a boolean iif theres an anagram present 2) count of anagram pairs
+
+// Psuedocode:
+// Iterate through the array of strings
+// node homework2-1.js
---
+++ js/interviewQs/summaryQ1-1.js
@@ -0,0 +1,15 @@
+// build a function that takes in an array of strngs and returns an array for each array item
+//it returns one array that has two items
+//one is a boolean - are there anagrams in this array
+//and the other is how many
+​
+//['raey', 'year'] -> [true, 1] // cause there are only one pair
+//return new array with 1st value being true and 2nd calue being 2/however many anagrams
+
+// Input an array of strings
+//  Output an array = [a boolean T/F if anagrams are present, count of anagrams ]
+// Psuedocode:
+   // Find if the string has anagrams
+   // Count the anagrams
+   // Based on Count, report T/F
+// node summaryQ1-1.js
