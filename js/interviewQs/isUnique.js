5fce5ba7e92a4e58f55ab735042a2a1ef4fba3ee
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-10-16 20:10:53 -0400

    commit

---
+++ js/interviewQs/isUnique.js
@@ -0,0 +1,4 @@
+// Does the string have unique characters?
+// Parameters "a string", check for length and error handling for types that are not strings
+// Return boolean of T or F
+// Example abbey false edwin true f true 432,false
