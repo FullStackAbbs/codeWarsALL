1b99540b0897b952b895add8a2277dde95ec9de4
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-10-24 18:10:33 -0400

    ok

---
+++ js/interviewQs/deleteMiddle.js
@@ -0,0 +1,9 @@
+Delete Middle Element from Array: Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last element, not necessarily the exact middle) of
+//
+// Paramters an Array
+// Return an array with the middle element remove
+// Example
+//  [0,1,2,3,4,5] --> [0,1,4,5]
+//  [0,1,2,3,4] --> [0,1,3,4]
+//
---
+++ js/interviewQs/theVowelCode.js
@@ -0,0 +1,45 @@
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+//
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+//
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+//
+// For example, decode("h3 th2r2") would return "hi there".
+//
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+// Paramters - string
+// Return - string with replaced values
+// Example
+// encode("hello") ---> "h2ll4"
+// Pscuedocode
+// create obj key value for letter and val
+// iterate through the string
+// if the string and check if matches / includes the obj key
+// replace the index with obj value of the respective key
+
+const vowelKeys = {
+  'a':'1',
+  'e':'2',
+  'i':'3',
+  'o':'4',
+  'u':'5'
+}
+
+function vowelCodeEncode(str){
+  for(i=0; i<str.length; i++){
+    if(Object.keys(vowelKeys).includes(str[i])){
+      str.replace(Object.keys(vowelKeys),'2')
+    }
+  }
+  return str
+}
+
+console.log(vowelCodeEncode('hello'),'h2ll4')
+
+// node theVowelCode.js
