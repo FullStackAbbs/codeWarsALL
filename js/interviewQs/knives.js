f281ac709dc136f9073189c08eabbe02978db2b8
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-12-10 16:00:38 -0500

    Update knives.js

--- js/interviewQs/knives.js
+++ js/interviewQs/knives.js
@@ -67,7 +67,6 @@ class Knife {
     this.location = location
     this.validCategory = validCategory
   }
-
   addToCollection(){
     knives.allknives.push({name:this.name, 
                           handle:this.handle, 
@@ -88,11 +87,15 @@ class Knife {
     return categoryCheck && psiCheck ? 'Can Cut' : 'Error: cannot cut '
     // sidenote: i wanted the categoryCheck as its own function but instead settled for the boolean check (line above)
     }
+  chop(arrFood){
+    const answer = arrFood.allfood.filter( el => el.psiSlice  - .15 < this.psi && el.psiSlice + .15 > this.psi).map(el => el.name)
+    return answer
+  }
 
-    chop(arrFood){
-      const answer = arrFood.allfood.filter( el => el.psiSlice  - .15 < this.psi && el.psiSlice + .15 > this.psi).map(el => el.name)
-      return answer
-    }
+
+
+
+  
 
   }
 
@@ -108,8 +111,7 @@ class Food {
     this.psiSlice = psiSlice
     this.spoilAge = spoilAge
     this.currentAge =currentAge
-//     The food class should have the properties you've mentioned here but also a spoilAge property and a currentAge property.
-// The user should be able to set currentAge AFTER making the object that's made from the food class. So maybe make a setter method  
+
   }
   isMoldy(){
     this.currentAge = this.currentAge++
@@ -120,45 +122,45 @@ class Food {
     }
 }}
 
-class Solution {
-    constructor(knives,food,choice){
-      this.knives=knives
-      this.food = food
-      this.choice = choice
-    }
-  reportKnife(result){
-  const msg = `You have selected ${this.choice} from the food list.The knife you will need is a ${result[0].name} and can be found in the knife drawer at location ${result[0].location}. Enjoy!  `
-  console.log(msg)
-  }
+// class Solution {
+//     constructor(knives,food,choice){
+//       this.knives=knives
+//       this.food = food
+//       this.choice = choice
+//     }
+//   reportKnife(result){
+//   const msg = `You have selected ${this.choice} from the food list.The knife you will need is a ${result[0].name} and can be found in the knife drawer at location ${result[0].location}. Enjoy!  `
+//   console.log(msg)
+//   }
 
-  findKnife(psiSliceSelected){
-    // psiSliceSelected (pounds per square inch) refers to the amount of pressure needed for a succesful cut
-    const tolerance = .15 // .15 psi range in order to find the correct knife
-    const upBound = psiSliceSelected + tolerance // the pressure need to make a cut while with tolerance range (high)
-    const lowBound = psiSliceSelected - tolerance // the pressure need to make a cut while with tolerance range (low)
-    let result = knives.allknives.filter( el =>  lowBound < el.psi && upBound > el.psi)  
-    this.reportKnife(result,this.choice)
-  }
+//   findKnife(psiSliceSelected){
+//     // psiSliceSelected (pounds per square inch) refers to the amount of pressure needed for a succesful cut
+//     const tolerance = .15 // .15 psi range in order to find the correct knife
+//     const upBound = psiSliceSelected + tolerance // the pressure need to make a cut while with tolerance range (high)
+//     const lowBound = psiSliceSelected - tolerance // the pressure need to make a cut while with tolerance range (low)
+//     let result = knives.allknives.filter( el =>  lowBound < el.psi && upBound > el.psi)  
+//     this.reportKnife(result,this.choice)
+//   }
 
-  findOnFoodList(){
-    let foodChoice = food.allfood.filter(el => el.name === this.choice)
-    let psiSliceSelected = foodChoice[0].psiSlice
-    this.findKnife(psiSliceSelected)
-  }
-
-  
-
-
-
-// find out what the user choose
-// match the choice text with food list
-// access the food list I need to grab the food psi Slice
-// filter the knife selection based on the foodPsislice matching the knives.psi value
-// return the location of the knife with the correct psi value
+//   findOnFoodList(){
+//     let foodChoice = food.allfood.filter(el => el.name === this.choice)
+//     let psiSliceSelected = foodChoice[0].psiSlice
+//     this.findKnife(psiSliceSelected)
+//   }
 
   
 
-}
+
+
+// // find out what the user choose
+// // match the choice text with food list
+// // access the food list I need to grab the food psi Slice
+// // filter the knife selection based on the foodPsislice matching the knives.psi value
+// // return the location of the knife with the correct psi value
+
+  
+
+// }
 
 
 
